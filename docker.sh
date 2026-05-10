4a07127d8a84bba128fd5c80d21116ece4d1f8af
vineela2025 <vineelakommojudevops@gmail.com>
2026-05-10 13:55:53 +0530

    updating files

--- docker.sh
+++ docker.sh
@@ -19,7 +19,7 @@ usermod -aG docker ec2-user
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
-kubectl version
+
 
 #eksctl
 ARCH=amd64
@@ -27,5 +27,5 @@ PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl  /usr/local/bin
-eksctl version
+
 
